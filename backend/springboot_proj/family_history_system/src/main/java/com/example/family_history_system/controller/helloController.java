f7de978d17937fe1d6949eceab1b7bf529a728f9
deney <xzjian2335672874@outlook.com>
2025-06-18 21:00:32 +0800

    后端

---
+++ backend/springboot_proj/family_history_system/src/main/java/com/example/family_history_system/controller/helloController.java
@@ -0,0 +1,18 @@
+package com.example.family_history_system.controller;
+import com.example.family_history_system.entity.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+public class helloController {
+    @RequestMapping(value="/hello",method=RequestMethod.GET)
+    public String hello(){
+        return "Hello World";
+    }
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    //localhost:8080/login?username=xia&password=123
+    public String log(String username, String password) {
+        return username + ":" + password;
+    }
+}
