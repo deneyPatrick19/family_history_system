f4cb605c8dfd6506ad56a0d3c9006c32bf7ddd1a
deney <xzjian2335672874@outlook.com>
2025-06-20 22:05:40 +0800

    前端修改

---
+++ frontend/vue_project/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div style="font-size: 20px; color: green; font-style: italic ;" >
+        HelloWorld!欢迎来到vue的世界!今天的调试完成了
+    </div>
+    <div>
+      {{ a }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+
+const a = ref("14")
+</script>
+
